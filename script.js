97b927aef2deeb8c9f86f29ef2daed07eac9bf41
RawJ123 <jairawat1325@gmail.com>
2025-12-27 05:44:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -33,3 +33,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+const heartsContainer = document.querySelector(".hearts");
+
+function createHeart() {
+  const heart = document.createElement("div");
+  heart.classList.add("heart");
+
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.animationDuration = 5 + Math.random() * 5 + "s";
+  heart.style.opacity = Math.random();
+
+  heartsContainer.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 10000);
+}
+
+setInterval(createHeart, 300);
